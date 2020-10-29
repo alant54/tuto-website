fd66de1bd2abddf89fd1cd8b032b35947e2f2a33
alant54 <64085439+alant54@users.noreply.github.com>
2020-10-29 13:31:42 +0100

    Update test.js

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ https://github.com/thiru112/CI4-Blog
 https://www.youtube.com/watch?v=0LrEsaMAxGA
 https://www.youtube.com/watch?v=R1StjWM_LOE
 https://www.tutorialspoint.com/codeigniter/index.htm
+https://codeigniter4.github.io/userguide/index.html
